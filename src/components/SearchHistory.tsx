ea7be684e3de59fde706d15cedd8b0ae19077ffe
Chequel McNeil <cmcne009@odu.edu>
2024-01-17 04:15:06 -0500

    Update searchhistory functionality

--- src/components/SearchHistory.tsx
+++ src/components/SearchHistory.tsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { useDispatch } from "react-redux";
-import { updatePokemonByName, selectDataByName } from "../redux/pokemonSlice";
+import { updatePokemonByName} from "../redux/pokemonSlice";
+import './styles/SearchHistory.css'
 
 export const SearchHistory = () => {
     const dispatch = useDispatch();
 
     const pokemonName = useSelector((state: RootState) => state.pokemon.name);
-    const data = useSelector((state: RootState) => selectDataByName(state, pokemonName));
 
     const [searchHistoryList, setSearchHistoryList] = useState<string[]>([]);
 
@@ -22,13 +22,19 @@ export const SearchHistory = () => {
     }
 
     return (
-        <div>
-            <ul >
-                {searchHistoryList.map((name, index) => {
-                    return(
-                    <li key={index} onClick={(e) => goToSelectedPokemon(e)}>{name}</li>
-                    )
-                })}
+        <div className="searchHistoryContainer">
+            <ul className="searchHistoryList">
+                <li>
+                    <input type="checkbox" id="list-item-1"></input>
+                    <label className="searchHistoryLabel" htmlFor="list-item-1">Search History</label>
+                    <ul className="searchHistoryItems">
+                        {searchHistoryList.map((name, index) => {
+                            return(
+                            <li key={index} className="historyItem" onClick={(e) => goToSelectedPokemon(e)}>{name}</li>
+                            )
+                        })}
+                    </ul>
+                </li>
             </ul>
         </div>
     )
